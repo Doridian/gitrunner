2536c16b9d15b200f824d5184f8e76a71a753106
Doridian <git@doridian.net>
2019-10-31 11:22:27 -0700

    port assignments

--- runner/index.js
+++ runner/index.js
@@ -305,7 +305,7 @@ http.createServer((req, res) => {
 
         res.writeHead(500, {'Content-Type': 'text/plain'});
         res.write(err.stack || err);
-        res.write('\n');
+        res.write('\n\n\n');
         res.write(stream.data);
         res.end();
     });
